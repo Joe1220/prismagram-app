13294e18d90f2941dac0a830df55e1d4d3d518a8
jsh901220 <joe1220@daum.net>
2019-08-01 19:20:00 +0900

    #12.0 AuthHome

--- components/NavController.js
+++ components/NavController.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { View } from "react-native";
-import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
+import { useIsLoggedIn } from "../AuthContext";
 import AuthNavigation from "../navigation/AuthNavigation";
 import MainNavigation from "../navigation/MainNavigation"
 
 export default () => {
-  const isLoggedIn = true
-  const logIn = useLogIn();
-  const logOut = useLogOut();
+  const isLoggedIn = useIsLoggedIn();
   return (
     <View style={{ flex: 1 }}>
       {isLoggedIn ? (
